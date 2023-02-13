deb6a349c54b5c19922d11744ae13c7b061bb04b
vijikumar123 <vk9164940756@gmail.com>
2023-02-13 11:44:47 +0530

    Added:uc1 Contact

--- src/main/java/com/bridgelabz/Contact.java
+++ src/main/java/com/bridgelabz/Contact.java
@@ -1,5 +1,26 @@
 package com.bridgelabz;
 
 public class Contact {
+    //setting the variables for the adress book
+    private String firstName;
+    private String lastName;
+    private String address;
+    private int zip;
+    private String phoneNumber;
+    private String email;
+
+    public Contact() {
+    }
+
+    //method to get the detailes
+    public Contact(String firstName, String lastName,String address,int zip, String email, String phoneNumber) {
+        //arching the encptulation
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address=address;
+        this.zip=zip;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
 
 }
